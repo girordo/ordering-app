49b704004e6b8a11a050caebb2272a6e238e6839
girordo <tsgiroldo@gmail.com>
2023-02-25 17:04:15 -0300

    feat(new libs): a lot of new stuff

--- apps/orders/src/dto/create-order.request.ts
+++ apps/orders/src/dto/create-order.request.ts
@@ -1,1 +1,18 @@
-export class CreateOrderRequest {}
+import {
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class CreateOrderRequest {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsPositive()
+  price: number;
+
+  @IsPhoneNumber()
+  phoneNumber: string;
+}
