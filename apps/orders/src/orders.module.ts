2f095abf564e352ad601acadcd6eb1c4b722affd
girordo <tsgiroldo@gmail.com>
2023-02-25 11:00:31 -0300

    feat(nestjs): new projectttttt

---
+++ apps/orders/src/orders.module.ts
@@ -0,0 +1,22 @@
+import { DatabaseModule } from '@app/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
+export class OrdersModule {}
