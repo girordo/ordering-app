9d235f70cdd3025018c4642b7a44f2b6760f0308
girordo <tsgiroldo@gmail.com>
2023-02-25 11:17:34 -0300

    feat(orders.repository) creating orders repository

--- apps/orders/src/schemas/order.schema.ts
+++ apps/orders/src/schemas/order.schema.ts
@@ -1,5 +1,16 @@
 import { AbstractDocument } from '@app/common';
-import { Schema } from '@nestjs/mongoose';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({ versionKey: false })
-export class Order extends AbstractDocument {}
+export class Order extends AbstractDocument {
+  @Prop()
+  name: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  phoneNumber: string;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
